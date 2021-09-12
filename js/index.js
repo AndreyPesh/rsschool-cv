b5f4646f01445379c700e8cbfbd03ed3572c937f
AndreyPesh <coursestud@mail.ru>
2021-09-12 19:22:46 +0300

    refactor: change styles according to codeguide

--- js/index.js
+++ js/index.js
@@ -2,9 +2,9 @@ window.onload = function() {
   const body = document.querySelector('body')
   const burger = document.querySelector('.burger')
   const navigationListHeader = document.querySelector('.navigation__header')
-  const widthDevice = window.innerWidth
   body.addEventListener('click', e => {
     const isBurger = e.target == burger || e.target.parentElement == burger
+    const widthDevice = window.innerWidth
     if (!burger.classList.contains('active') && isBurger  && widthDevice <= 768) {
       navigationListHeader.classList.add('active')
       burger.classList.add('active')
@@ -13,6 +13,17 @@ window.onload = function() {
         navigationListHeader.classList.remove('active')
         burger.classList.remove('active')
         body.classList.remove('lock')
+    }
+  })
+  window.addEventListener('resize', () => {
+    if (window.innerWidth > 768 && body.classList.contains('lock')) {
+      body.classList.remove('lock')
+    }
+    if (burger.classList.contains('active')) {
+      burger.classList.remove('active')
+    }
+    if (navigationListHeader.classList.contains('active')) {
+      navigationListHeader.classList.remove('active')
     }
   })
 }
